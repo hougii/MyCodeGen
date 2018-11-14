d28d0da9a97cbfaa82991614f6c99d0e91c3e10a
howard Chiang <hougii@gmail.com>
2018-11-14 21:46:28 +0800

    增加產生Markdown格式區域

---
+++ CodeGen.Web/Utility/MarkdownGenerator.cs
@@ -0,0 +1,18 @@
+﻿using CodeGen.Web.Models;
+using DotLiquid;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGen.Web.Utility
+{
+    public class MarkdownGenerator : BaseGenerator
+    {
+        public override string LiquidPath => "\\template\\Markdown\\Markdown.liquid";
+
+    }
+}
